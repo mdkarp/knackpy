468301792e0867e8d1e4ad6a8212d84e47d7dc55
John Clary <john.clary@austintexas.gov>
2019-09-12 01:25:41 -0500

    new tests

--- knackpy/tests/test_knackpy.py
+++ knackpy/tests/test_knackpy.py
@@ -1,102 +1,77 @@
-app_id='your_api_key'
-api_key='your_app_id'
-        
-class TestClass:
-    def setUp(self):
-        self.apps = {
-            'scene_view_with_api_key' :
-                {   
-                    #  view-based request with api_key
-                    'scene' : 'scene_73',
-                    'view' : 'view_197',
-                    'ref_obj' : ['object_12', 'object_11'],
-                    'app_id' : app_id,
-                    'api_key' : api_key,
-                    'page_limit' : 1,
-                    'rows_per_page' : 10
-                },
-            'scene_view_no_api_key' :
-                {   
-                    #  view-based request without api_key
-                    'scene' : 'scene_467',
-                    'view' : 'view_1329',
-                    'ref_obj' : ['object_31'],
-                    'app_id' : app_id,
-                    'page_limit' : 1,
-                    'rows_per_page' : 10
-                },
-            'object_api_key' :
-            {   
-                #  object-based request
-                'obj' : 'object_12',
-                'app_id' : app_id,
-                'api_key' : api_key,
-                'page_limit' : 1,
-                'rows_per_page' : 10
-            }
+import pytest
 
-        }
+from test_secrets import AUTH
+from test_config import APP
+
+"""
+Tests:
+- [x] view + scene 
+- [x] object
+- both view + scene + object
+- not view + scene or object
+- public view/scene
+- all params
+- all methods
+- datestings
+- each fieldtype
+- mutilple attachments
+- downloads
+- insert
+- update
+- delete (need this method)
+
+"""
 
 
-    def tearDown(self):
-        #  for filename in self.filenames:
-        #       delete filename
-        return
-
+class TestKnackpy:
     def test_scene_view_with_api_key(self):
+
         from knackpy import Knack
 
-        app = self.apps['scene_view_with_api_key']
+        module = APP["all_fields"]
 
         kn = Knack(
-            scene=app['scene'],
-            view=app['view'],
-            ref_obj=app['ref_obj'],
-            app_id=app['app_id'],
-            api_key=app['api_key'],
-            page_limit=2,
-            rows_per_page=10
-        )
-
-        assert len(kn.data) == 20
-
-    def test_scene_view_no_api_key(self):
-        from knackpy import Knack
-
-        app = self.apps['scene_view_no_api_key']
-
-        kn = Knack(
-            scene=app['scene'],
-            view=app['view'],
-            app_id=app['app_id'],
+            scene=module["scene"],
+            view=module["view"],
+            ref_obj=module["ref_obj"],
+            app_id=AUTH["app_id"],
+            api_key=AUTH["api_key"],
             page_limit=1,
-            rows_per_page=10
+            rows_per_page=1,
         )
 
-        assert len(kn.data_raw) > 0
+        assert len(kn.data) > 0
 
     def test_object_api_key(self):
+
         from knackpy import Knack
 
-        app = self.apps['object_api_key']
+        module = APP["all_fields"]
 
         kn = Knack(
-            obj=app['obj'],
-            app_id=app['app_id'],
-            api_key=app['api_key'],
-            page_limit=2,
-            rows_per_page=10
+            obj=module["obj"],
+            app_id=AUTH["app_id"],
+            api_key=AUTH["api_key"],
+            page_limit=1,
+            rows_per_page=1,
         )
 
-        assert len(kn.data) == 20
+        assert len(kn.data) > 0
 
+    def test_object_view_scene_error(self):
+        from knackpy import Knack
 
+        module = APP["all_fields"]
 
+        with pytest.raises(Exception) as excinfo:
 
-
-
-
-
-
-
-
+            assert Knack(
+                obj=module["obj"],
+                scene=module["scene"],
+                view=module["view"],
+                app_id=AUTH["app_id"],
+                api_key=AUTH["api_key"],
+                page_limit=1,
+                rows_per_page=1,
+            )
+            assert "not both" in excinfo.lower()
