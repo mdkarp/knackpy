ff3b5f0bf904469e4e07720fa88c2fe09782e050
John Clary <john.clary@austintexas.gov>
2019-09-12 20:57:38 -0500

    refactoring for better tests

---
+++ knackpy/tests/test_config.py
@@ -0,0 +1,15 @@
+APP = {
+    "all_fields": {
+        "obj": "object_3",
+        "scene": "scene_1",
+        "view": "view_6",
+        "ref_obj": ["object_3", "object_2"],
+    },
+    "parent": {"obj": "object_2", "scene": "scene_1", "view": "view_1", "ref_obj": []},
+    "many_to_many": {
+        "obj": "object_4",
+        "scene": "scene_1",
+        "view": "view_1",
+        "ref_obj": [],
+    },
+}
