18fdc5d81962109a7084630447e7110583afe663
John Clary <john.clary@austintexas.gov>
2019-09-12 22:04:32 -0500

    in progress..

--- knackpy/tests/test_knackpy.py
+++ knackpy/tests/test_knackpy.py
@@ -1,4 +1,5 @@
-import logging
+import pytest
+import pdb
 
 from test_secrets import AUTH
 from test_config import APP
@@ -21,30 +22,107 @@ Tests:
 - update
 - delete (need this method)
 - csv
+- insert connected record
+- populate connection field
 """
-
-import pdb
 
 class TestKnackpy:
 
-    def test_record_upload(self):
-        from knackpy import Knack
+    def test_record_update_datetime(self):
+        import knackpy
+        import datetime
 
         module = APP["all_fields"]
 
-        kn = Knack(
-            scene=module["scene"],
-            view=module["view"],
-            ref_obj=module["ref_obj"],
+        now = int(datetime.datetime.now().timestamp()) * 1000
+        
+        # this updates the record with a current *UTC* timestamp into PYTEST_MODIFIED_DATE
+        record = {
+            'field_37': now, # PYTEST_MODIFIED_DATE
+            'id': '5d7964422d7159001659b27a'
+        }
+
+        res = knackpy.record(
+            record,
+            obj_key=module["obj"],
             app_id=AUTH["app_id"],
             api_key=AUTH["api_key"],
-            page_limit=1,
-            rows_per_page=1,
+            id_key="id",
+            method="update"
         )
 
-        pdb.set_trace()
-        assert 1==1
+        now_knack = res["field_37_raw"]["unix_timestamp"]
 
+        # check that updated value is within 1 minute (60k milliseconds) of update
+        # (Knack rounds to nearest minute)
+        assert abs(now - now_knack) < 60000
+
+
+    def test_record_insert_datetime(self):
+        import knackpy
+        import datetime
+
+        module = APP["all_fields"]
+
+        now = int(datetime.datetime.now().timestamp()) * 1000
+        
+        # this inserts the record with a current *UTC* timestamp into PYTEST_MODIFIED_DATE
+        record = {
+            'field_37': now, # PYTEST_MODIFIED_DATE
+            'field_38' : True # PYTEST_TEST_RECORD
+        }
+
+        res = knackpy.record(
+            record,
+            obj_key=module["obj"],
+            app_id=AUTH["app_id"],
+            api_key=AUTH["api_key"],
+            id_key="id",
+            method="create"
+        )
+
+        now_knack = res["field_37_raw"]["unix_timestamp"]
+
+        pdb.set_trace()
+        # check that updated value is within 1 minute (60k milliseconds) of update
+        # (Knack rounds to nearest minute)
+        assert abs(now - now_knack) < 60000
+
+
+    def test_record_delete(self):
+        """
+        I have no idea how to *unit test* a record delete. I guess we could insert
+        records in the setup? What if the setup fails? Hrumph.
+        Setup seems like the right way to handle this.
+        This means using pytest fixtures.
+        """
+        import knackpy
+        
+        module = APP["all_fields"]
+
+        # get all knackpy test records
+
+        kn = Knack(
+            obj=module["obj"],
+            app_id=AUTH["app_id"],
+            api_key=AUTH["api_key"]
+        )
+
+        res = knackpy.record(
+            record,
+            obj_key=module["obj"],
+            app_id=AUTH["app_id"],
+            api_key=AUTH["api_key"],
+            id_key="id",
+            method="create"
+        )
+
+        now_knack = res["field_37_raw"]["unix_timestamp"]
+
+        pdb.set_trace()
+        # check that updated value is within 1 minute (60k milliseconds) of update
+        # (Knack rounds to nearest minute)
+        assert abs(now - now_knack) < 60000
 
     def test_scene_view_with_api_key(self):
         # create a Knack instance from a scene key + view key
